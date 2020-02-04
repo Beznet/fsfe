726a87a22e6e8e2ad40859e69a32d72646aa4890
Bennett Dungan <bennettdungan@gmail.com>
2020-02-03 20:53:18 -0600

    418 error page

--- app.js
+++ app.js
@@ -8,4 +8,10 @@ app.get('/', (req, res) => {
 
 });
 
+app.get('/bonk', (req, res) => {
+	res.set('X-full-stack', 'bois');
+	res.status(418);
+	res.send('I prefer coffee');
+});
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
